eec4568cd16748a636794c1030b2c6ca9eecf5df
AdamMzkr <adm.mzkr@gmai.com>
2022-01-13 12:48:15 +0100

    create card component to represented each robots

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Card=({id,name,email})=>{
+	// const {id,name,email}=props;
+	return(
+			<div class="bg-dark-green washed-green pa3 dib tc ma2 br3 grow bw2 shadow-3">
+				<img alt='robot' src ={`https://robohash.org/${id}?size=200x200`}/>
+					<div>
+						<h2>{name}</h2>
+						<p>{email}</p>
+					</div>
+			</div>
+		);
+}
+
+export default Card;
