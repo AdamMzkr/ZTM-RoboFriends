bf1671bfd71b35add41511d3a0790bc477013324
AdamMzkr <adm.mzkr@gmai.com>
2022-01-18 20:56:15 +0100

    add search interactive input to filter result with value in input

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card=({id,name,email})=>{
 	// const {id,name,email}=props;
 	return(
-			<div class="bg-dark-green washed-green pa3 dib tc ma2 br3 grow bw2 shadow-3">
+			<div className="bg-dark-green washed-green pa3 dib tc ma2 br3 grow bw2 shadow-3">
 				<img alt='robot' src ={`https://robohash.org/${id}?size=200x200`}/>
 					<div>
 						<h2>{name}</h2>
